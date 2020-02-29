25f4de42b5bc544560a1aac10e80cce30e2fcabb
NH <ridoyhossain5557@gmail.com>
2020-02-29 05:33:37 -0800

    Course1

---
+++ course1.js
@@ -0,0 +1,14 @@
+
+//getElementById()
+// getElementsByTagName()
+//getElementsByClassName()
+
+
+let heading1 = document.getElementById('heading1');
+let para = document.getElementById('para');
+let heading2 = document.getElementById('heading2');
+
+heading2.innerHTML = 'Good Bye';
+para.innerHTML = 'I am fine and you';
+heading1.innerHTML = 'Hi, how are you';
+
