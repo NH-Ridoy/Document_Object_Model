959313163f31f1cf957313a6a9edc6019f846961
NH <ridoyhossain5557@gmail.com>
2020-02-29 21:05:43 -0800

    Style_your_Document

--- course2.js
+++ course2.js
@@ -1,11 +1,11 @@
 
-let btn = document.querySelector('button');
+let btn = $('button');
 let colors = ['green', 'red', 'silver', 'orange'];
 
 let value = 0;
 
 btn.addEventListener('click',() =>{
-    let h1 = document.querySelector('h1')
+    let h1 = $('h1')
     h1.innerText = 'Javascript';
 
     h1.style.color = colors[value];
@@ -17,3 +17,29 @@ btn.addEventListener('click',() =>{
     }
 
 });
+
+function $(selector) {
+    return document.querySelector(selector);
+}
+
+
+
+let hStyle = {
+    background: 'salmon',
+    fontSize: '30px',
+    fontFamily: 'Arial',
+    textAlign: 'center',
+    color: '#222',
+    padding: '20px'
+}
+
+Object.assign($('h1').style, hStyle);
+
+let pStyle = {
+    padding: '10px',
+    fontSize: '18px',
+    color: '#333333',
+    lineHeight: '1.3'
+}
+
+Object.assign($('p').style, pStyle);
